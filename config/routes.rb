eb206337107ef080d2f036b395722433f488fe38
Giorgio Zuccarelli <giorgio.zuccarelli21@avenues.org>
2021-04-10 16:15:54 -0300

    task manager final version

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # List all tasks
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: 'tasks'
 
   # Create forms
   get 'tasks/new', to: 'tasks#new'
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
 
   #edit form
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 
   #update the task
   patch 'tasks/:id', to: 'tasks#update'
